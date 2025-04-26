2c62089d2de4aeb3a97b8c760d0d7c982fa29911
= <=>
2025-04-26 18:29:36 -0500

    Refactor project structure by removing all Jupyter notebooks, data files, and utility scripts to streamline the repository and facilitate a fresh start. This cleanup reduces clutter and prepares the project for future development.

---
+++ constants.py
@@ -0,0 +1,30 @@
+initial_dir = "./datos/1. inicial"
+tratados_dir = "./datos/2. tratados"
+converted_dir = "./datos/3. convertidos"
+separated_dir = "./datos/4. separados"
+scaled_dir = "./datos/5. escalados"
+predicted_dir = "./datos/6. predichos"
+models_dir = "./models"
+
+# Predictor variables
+predictor_vars = [
+    "anio_registro",
+    "millas_recorridas",
+    "precio_vehiculo",
+    "num_asientos",
+    "num_puertas",
+    "complejidad_reparacion",
+    "costo_reparacion",
+    "horas_reparacion",
+    "marca2_Audi",
+    "marca2_BMW",
+    "marca2_Citroen",
+    "marca2_Ford",
+    "marca2_Nissan",
+    "marca2_OTROS",
+    "marca2_Peugeot",
+    "marca2_Toyota",
+    "marca2_Vauxhall",
+    "marca2_Volkswagen",
+]
+target_var = "fraude"
